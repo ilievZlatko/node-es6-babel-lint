c5c70ac36da1aa1f39e7c9af197d68f27f9460bf
Zlatko Iliev <iliev.zlatko@gmail.com>
2018-10-09 22:30:28 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import message from './message';
+
+const printOut = () => message;
+
+printOut();
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
